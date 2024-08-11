514d8a2af34598f7beee9fa0d316775d7a9c9cfa
Jordan Covilla <jordancovilla@hotmail.com>
2024-08-10 22:37:47 -0500

    organizacion de carpetas y creacion de index.html

--- App/App.py
+++ App/App.py
@@ -1,18 +1,9 @@
-
-from flask import Flask
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def gsdg():
-    return '¡Hola, Hoy es sabados'
-
-@app.route('/F')
-def Fauna():
-    return render_template ('Fauna.html')
-@app.route('/Fl')
-def Flor():
-    return render_template ('Flora.html')
+def index():
+    return render_template ('index.html')
   
 if __name__ == '__main__':
     app.run(debug=True)
